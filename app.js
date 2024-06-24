80ebd5ec2ee8ceb03d863d3c4dd29888bb9ad34b
erkansevim <erkansevim.essgmail.com>
2024-06-24 10:28:37 +0200

    setintervel

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 setTimeout(() => {
     console.log("I am Erkan");
-}, 2000);
+}, 200);
 
 console.log("who are you");
+
+
+//---------
+
+let sayac=0
+const delayed=setInterval(() => {
+    console.log(++sayac);
+
+    if (sayac==7) {
+        clearInterval(delayed)
+    }
+}, 1000);
+
+//---------------------
+
